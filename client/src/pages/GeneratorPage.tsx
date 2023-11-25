a8e5436316b5aeaa5b1eee4e9f0087a158bafbde
Elena Gonzalez <egonzalezcm08@gmail.com>
2023-11-25 17:48:36 +0100

    feat: create Form component

--- client/src/pages/GeneratorPage.tsx
+++ client/src/pages/GeneratorPage.tsx
@@ -1,4 +1,15 @@
+import NavOutlet from '../components/NavOutlet';
+import Form from '../components/Form';
+
 const GeneratorPage = () => {
-  return <div>GeneratorPage</div>;
+  return (
+    <div>
+      <h1>Generate an activity</h1>
+      <p>Based on your preferences:</p>
+      <Form />
+      {/* BUTTON */}
+      <NavOutlet />
+    </div>
+  );
 };
 export default GeneratorPage;
