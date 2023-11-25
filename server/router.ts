fdbfa096aa0d0af83cca8106e428da418353d81b
Zarina Saidova Poyet <zarina.poyet@gmail.com>
2023-11-25 17:35:26 +0100

    feat: user can update their username and password

--- server/router.ts
+++ server/router.ts
@@ -1,5 +1,11 @@
 import { Router } from 'express';
-import { login, register, updateUserAvatar } from './controllers/users';
+import {
+  login,
+  register,
+  updateUserAvatar,
+  updateUsername,
+  updatePassword,
+} from './controllers/users';
 import { getAllUsers } from './middlewares/users';
 import { isAuthenticated, isOwner } from './middlewares';
 
@@ -9,5 +15,17 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/users', isAuthenticated, getAllUsers);
 router.put('/users/:id', isAuthenticated, isOwner, updateUserAvatar);
+router.put(
+  '/users/user_username/:id',
+  isAuthenticated,
+  isOwner,
+  updateUsername
+);
+router.put(
+  '/users/user_password/:id',
+  isAuthenticated,
+  isOwner,
+  updatePassword
+);
 
 export default router;
