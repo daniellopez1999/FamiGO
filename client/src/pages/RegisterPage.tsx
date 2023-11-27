b4a5d1c269079beacd5cc38b2c36a4cbaf360663
Daniel López Bogaz <lopezbogazdaniel@gmail.com>
2023-11-27 15:25:50 +0100

    feat: register form partially done, pending create API Call in apiService and import into RegisterPage.tsx

--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -1,4 +1,46 @@
+import { useState } from 'react';
+import { User } from '../types/user';
+
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  interface FormState {
+    inputValues: User;
+  }
+
+  const [inputValues, setInputValues] = useState<FormState['inputValues']>({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValues({
+      ...inputValues,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault;
+
+    if (inputValues.password != inputValues.confirmPassword) {
+      console.error('Password is different than Confirm Password');
+    } else {
+      registerUser(inputValues).then(() => {
+        setInputValues({
+          username: '',
+          email: '',
+          password: '',
+          confirmPassword: '',
+        });
+      });
+    }
+  };
+
+  return (
+    <div>
+      <div>RegisterPage</div>
+    </div>
+  );
 };
 export default RegisterPage;
