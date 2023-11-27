dadf21f6d72f55aef74a5759e7e7e9a61b70fc47
Elena Gonzalez <egonzalezcm08@gmail.com>
2023-11-27 18:50:21 +0100

    feat: request openai and mock data

---
+++ server/controllers/generateActivity.ts
@@ -0,0 +1,80 @@
+import openAIResponse from '../mocks/openAIGeneratedActivity.json';
+import { Request, Response } from 'express';
+
+export const generateActivity = async (req: Request, res: Response) => {
+  const filtersBody = req.body;
+  console.log('filtersBody', filtersBody);
+
+  console.log('openAIResponse', openAIResponse);
+  res.json({ openAIResponse });
+};
+
+// import OpenAI from 'openai';
+// import { Request, Response } from 'express';
+
+// const openai = new OpenAI({
+//   apiKey: process.env.VITE_OPENAI_API_KEY,
+// });
+
+// interface IResponseFromAPI {
+//   filters: Object;
+//   title: String;
+//   materials: Array<String>;
+//   description: String;
+// }
+
+// export const generateActivity = async (req: Request, res: Response) => {
+//   const filtersBody = req.body;
+//   const Topic = filtersBody.Topic.value;
+// const KidsNumber = filtersBody.KidsNumber.value;
+// const AgeRange = filtersBody.AgeRange.value;
+// const Difficulty = filtersBody.Difficulty.value;
+// const Place = filtersBody.Place.value;
+// const Duration = filtersBody.Duration.value;
+
+//   console.log('filtersBody', filtersBody);
+
+//   const requestToAPI = `'You are an assistant that design activities taking into account these filters:
+//   - the topic of the activity: ${Topic};
+//   - the number of kids that are going to play: ${KidsNumber};
+//   - the age range of the kids that are going to play: ${AgeRange};
+//   - the difficulty of the activity (beginner, intermediate or expert): ${Difficulty};
+//   - the place (outside or inside): ${Place};
+//   - the duration of the activity: ${Duration}
+
+//   Your answer needs to be in json format and needs to be parseable with javascript:
+//   {
+//     filters,
+//     title,
+//     materials,
+//     description,
+//   }
+
+//   Here's an explanation of each field of the response:
+
+//   Give me an activity structured in these parts:
+//   - filters: List all the filters passed.
+//   - title
+//   - materials: Everything that is needed for the activity.
+//   - description: A short description of the activity that has to be maximum 500 characters long.'`;
+
+//   try {
+//     const completion = await openai.chat.completions.create({
+//       messages: [{ role: 'system', content: `${requestToAPI}` }],
+//       model: 'gpt-4',
+//       max_tokens: 500,
+//     });
+
+//     const content = completion.choices[0].message.content;
+//     console.log(completion.choices[0].message.content);
+
+//     if (content === null) {
+//       throw new Error('Received null content from API');
+//     }
+
+//     const responseFromAPI: IResponseFromAPI = JSON.parse(content);
+//     res.json(responseFromAPI);
+//   } catch (err) {
+//     console.error('Error:', err);
+//   }
+// };
