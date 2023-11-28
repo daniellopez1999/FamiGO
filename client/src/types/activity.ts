40f5c2b21f4a7e612bd9b7f5696da64592cf8f11
wangjianing <jianingmica@gmail.com>
2023-11-28 19:54:02 +0100

    feat: add type for activity form

--- client/src/types/activity.ts
+++ client/src/types/activity.ts
@@ -8,3 +8,20 @@ export interface OptionGroup<Option> {
   name: string;
   placeholder: string;
 }
+
+export interface FiltersWithValues {
+  topic: string;
+  numOfKids: string;
+  age: string;
+  difficulty: string;
+  place: string;
+  duration: string;
+}
+
+export interface Activity {
+  image?: string;
+  filters: FiltersWithValues;
+  title: string;
+  materials: string[];
+  description: string;
+}
