7b114a3ebaf8bbe528bc1d685264b1da0e8245d2
wangjianing <jianingmica@gmail.com>
2023-11-30 17:22:58 +0100

    chore: remove unused type

--- server/types.d.ts
+++ server/types.d.ts
@@ -1,25 +1,3 @@
-export interface IUser {
-  _id: string;
-  username: string;
-  email: string;
-  password: string;
-  avatar: string;
-  followers?: [string];
-  following?: [string];
-  posts?: [string];
-  savedPosts?: [string];
-}
-
-export interface UserData {
-  statistics: UserStatistics;
-  _id: string;
-  username: string;
-  email: string;
-  avatar: string;
-  savedPosts: any[];
-  __v: number;
-}
-
 export interface UsersData {
   [key: string]: {
     statistics: UserStatistics;
