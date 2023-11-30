93a88879289b555955cc752b4664f5243c50462f
wangjianing <jianingmica@gmail.com>
2023-11-30 17:25:00 +0100

    feat: middleware for authorization

--- server/middlewares/index.ts
+++ server/middlewares/index.ts
@@ -57,6 +57,26 @@ export const isAuthenticated = async (
   }
 };
 
+// should be used after isAuthenticated middleware
+export const isAuthorized = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { identity } = req as RequestWithUser;
+    const { username } = req.cookies;
+
+    if (identity.username !== username) {
+      res.status(401).end(); // 401 Unauthorized
+    }
+
+    return next();
+  } catch (error) {
+    res.status(400).end();
+  }
+};
+
 export const cookiesOK = async (_req: Request, res: Response) => {
   res.sendStatus(200);
 };
