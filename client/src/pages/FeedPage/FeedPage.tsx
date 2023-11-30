3d37bc4d799d2e2e8214b71c62c700b8eadde839
Elena Gonzalez <egonzalezcm08@gmail.com>
2023-11-30 10:05:45 +0100

    feat: add filter option in feed (#73)

--- client/src/pages/FeedPage/FeedPage.tsx
+++ client/src/pages/FeedPage/FeedPage.tsx
@@ -2,12 +2,26 @@ import FeedItem from '../../components/FeedItem/FeedItem';
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { checkAuthentication } from '../../services/auth';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 import './FeedPage.css';
+import FiltersSelect from '../../components/FiltersSelect/FiltersSelect';
+
+export interface IFormInput {
+  Topic: {};
+  KidsNumber: {};
+  AgeRange: {};
+  Difficulty: {};
+  Place: {};
+  Duration: {};
+}
 
 const FeedPage = () => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   const [hasChecked, setHasChecked] = useState<boolean>(false);
+  const [postsByFilter, setPostsByFilter] = useState();
+
+  const { control, handleSubmit } = useForm<IFormInput>({});
 
   const navigate = useNavigate();
 
@@ -26,11 +40,36 @@ const FeedPage = () => {
     }
   }
 
+  const onSubmit: SubmitHandler<any> = (data) => {
+    const apiEndpoint = 'http://localhost:3000/feed';
+    console.log('Sending Request with Data:', data);
+
+    fetch(apiEndpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((dataReceived) => {
+        console.log('Data Received:', dataReceived);
+        setPostsByFilter(dataReceived);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
   return (
     <div className="feed-page">
       {hasChecked && isAuthenticated && (
         <>
           Filter Placeholder
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <FiltersSelect control={control} />
+            <button type="submit">Search</button>
+          </form>
           <FeedItem />
           <FeedItem />
           <FeedItem />
