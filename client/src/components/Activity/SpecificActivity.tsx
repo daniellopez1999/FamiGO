51ae7b6a9bf1a0def4a5d869592599320f57d46a
Daniel López Bogaz <lopezbogazdaniel@gmail.com>
2023-11-30 11:00:02 +0100

    feat: acativity is being shown compleely

--- client/src/components/Activity/SpecificActivity.tsx
+++ client/src/components/Activity/SpecificActivity.tsx
@@ -2,10 +2,13 @@ import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { getActivity } from '../../services/activity';
 import { ActivityObject } from '../../types/activity';
+import { getUserInfo } from '../../services/users';
+import { UserInfo } from '../../types/user';
 
 const SpecificActivity = () => {
   const { id } = useParams();
   const [activityData, setActivityData] = useState<ActivityObject | null>(null);
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
 
   useEffect(() => {
     async function getActivityInfo() {
@@ -16,12 +19,35 @@ const SpecificActivity = () => {
   }, [id]);
 
   useEffect(() => {
-    console.log(activityData);
-  });
+    const username = activityData?.activityInfo.userInfo.username;
+    async function getInfoFromUser() {
+      const userData = await getUserInfo(username!);
+      setUserInfo(userData);
+    }
+    getInfoFromUser();
+  }, []);
 
   return (
-    <div>
-      <img src={activityData?.activityInfo.image} />
+    <div className="feed-item">
+      <div className="info">
+        <div className="avatar">
+          <img src={userInfo?.user.avatar} alt="avatar" />
+        </div>
+        <p>{activityData?.activityInfo.userInfo.username}</p>
+      </div>
+      <div className="content">
+        <img src={activityData?.activityInfo.image} alt="activity image" />
+      </div>
+      <div className="status">
+        <p>{activityData?.activityInfo.likes.length} likes</p>
+        <div className="actions">
+          <p>like</p>
+          <p>comment</p>
+          <p>save</p>
+        </div>
+      </div>
+      <p>{activityData?.activityInfo.description}</p>
+      <p>view all comments</p>
     </div>
   );
 };
