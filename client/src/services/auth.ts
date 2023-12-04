7abba6d3e05d7cfb17291aaa7f962b41c9428ed9
Zarina Saidova Poyet <zarina.poyet@gmail.com>
2023-12-04 15:57:35 +0100

    feat: service for reset password

--- client/src/services/auth.ts
+++ client/src/services/auth.ts
@@ -110,3 +110,20 @@ export const sendPasswordResetEmail = async (email: string) => {
     throw error;
   }
 };
+
+export const resetPassword = async (token: string, newPassword: string) => {
+  try {
+    const response = await fetch(`${BASE_URL}/reset-password`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ token, newPassword }),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Reset password failed', error);
+    throw error;
+  }
+};
