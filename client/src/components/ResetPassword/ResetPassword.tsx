effd81ec95387bc86d5a00ce027ca63938cc8972
Zarina Saidova Poyet <zarina.poyet@gmail.com>
2023-12-04 13:30:32 +0100

    feat: basic structure for ResetPassword page

--- client/src/components/ResetPassword/ResetPassword.tsx
+++ client/src/components/ResetPassword/ResetPassword.tsx
@@ -1,5 +1,42 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const ResetPassword = () => {
-  return <div></div>;
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("Password don't match");
+      return;
+    }
+
+    // Todo: Send new password to server
+    navigate('/login');
+  };
+
+  return (
+    <div>
+      <h1>New Password</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="password"
+          value={password}
+          placeholder="Create new password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password"
+          value={confirmPassword}
+          placeholder="Confirm your password"
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default ResetPassword;
