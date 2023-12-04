b88b61f227c5fbfb29d10fc94b82628c7f933272
Zarina Saidova Poyet <zarina.poyet@gmail.com>
2023-12-04 13:15:18 +0100

    feat: basic structure for ForgotPassword page

--- client/src/components/ForgotPassword/ForgotPassword.tsx
+++ client/src/components/ForgotPassword/ForgotPassword.tsx
@@ -1,5 +1,34 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const ForgotPassword = () => {
-  return <div></div>;
+  const [email, setEmail] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // Todo: Send a request to server
+    // ? await sendPasswordResetEmail(email);
+
+    navigate('/reset-pasword');
+  };
+
+  return (
+    <div>
+      <h1>Forgot Password</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          value={email}
+          placeholder="Enter you email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="submit">Continue</button>
+      </form>
+    </div>
+  );
 };
 
 export default ForgotPassword;
