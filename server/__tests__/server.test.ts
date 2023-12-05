b2c045d7afb4dfa9b752d19536efbbdc088fe8ce
Daniel López Bogaz <lopezbogazdaniel@gmail.com>
2023-12-05 16:45:53 +0100

    feat: Save / Unsave post test working

--- server/__tests__/server.test.ts
+++ server/__tests__/server.test.ts
@@ -12,6 +12,12 @@ const activityID = {
   1: '656f38edde117c8d7ad025e7',
 };
 
+const activityIDToSave = '6569fd7d1fb3a9b099125914';
+
+const username = 'dani2';
+
+var profileData: any = {};
+
 var cookie = '';
 
 beforeAll(async () => {
@@ -20,7 +26,6 @@ beforeAll(async () => {
   try {
     const response = await request(app).post('/login').send(credentials);
     expect(response.status).toBe(200);
-
     cookie = response.headers['set-cookie'];
   } catch (error) {
     console.error(error);
@@ -54,7 +59,6 @@ describe('Activity', () => {
         .get(`/get-activity/${activityID[0]}`)
         .set('Cookie', cookie)
         .send();
-      console.log(response.body.activityInfo);
       expect(response.status).toBe(200);
       expect(response.body.activityInfo.description).toBe('7w7');
     } catch (error) {
@@ -69,7 +73,6 @@ describe('Activity', () => {
         .get(`/get-activity/${activityID[1]}`)
         .set('Cookie', cookie)
         .send();
-      console.log(response.body.activityInfo);
       expect(response.status).toBe(200);
       expect(response.body.activityInfo.description).toBe('willy wonka');
     } catch (error) {
@@ -78,11 +81,45 @@ describe('Activity', () => {
     }
   });
 
-  test('Check if Activity is Saved or Unsaved correctly', async () => {
+  test('Get my profile data', async () => {
     try {
+      const response = await request(app)
+        .get(`/user/${username}`)
+        .set('Cookie', cookie)
+        .send();
+      expect(response.status).toBe(200);
+
+      profileData = response.body;
     } catch (error) {
       console.error(error);
       throw error;
+    }
+  });
+
+  //If post is saved, unsave it, will return 201. If it's not saved it, save it, will return 200
+  test('Check if Activity is Saved or Unsaved correctly', async () => {
+    if (profileData.savedPosts.includes(activityIDToSave)) {
+      try {
+        const response = await request(app)
+          .post(`/savepost-in-user/${username}/${activityIDToSave}`)
+          .set('Cookie', cookie)
+          .send();
+        expect(response.status).toBe(201);
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    } else {
+      try {
+        const response = await request(app)
+          .post(`/savepost-in-user/${username}/${activityIDToSave}`)
+          .set('Cookie', cookie)
+          .send();
+        expect(response.status).toBe(200);
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
     }
   });
 });
